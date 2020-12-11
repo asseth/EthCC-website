2d3400c56adf64fcbc187b812556fbfebd251386
pscott <scottpiriou@gmail.com>
2020-12-11 15:33:09 +0100

    Update speakers 11 Dec

--- speakers-json.js
+++ speakers-json.js
@@ -1,30 +1,213 @@
 var speakers = [
-  {
-    "firstName": "Vitalik",
-    "lastName": "Buterin",
-    "link": "https://twitter.com/VitalikButerin",
-    "picture": "images/speakers_2020/Vitalik_Buterin.jpg",
-    "company": "Ethereum Foundation"
-  },
-  {
-    "firstName": "Joseph",
-    "lastName": "Lubin",
-    "link": "https://twitter.com/ethereumJoseph",
-    "picture": "images/speakers_2020/Joe_Lubin.png",
-    "company": "ConsenSys"
-  },
-  {
-    "firstName": "Evan",
-    "lastName": "Van Ness",
-    "link": "https://twitter.com/evan_van_ness",
-    "picture": "images/speakers_2020/Evan_VanNess.jpeg",
-    "company": "ConsenSys"
-  },
-  {
-    "firstName": "Martin",
-    "lastName": "lundfall",
-    "link": "https://twitter.com/martinlundfall",
-    "picture": "images/speakers_2020/Martin_Lundfall.jpg",
-    "company": "Ethereum Foundation"
-  },
+[
+    {
+        "company": "Byzantine Fault",
+        "first_name": "Afri",
+        "last_name": "Schoedon",
+        "link": "https://twitter.com/q9fmz",
+        "picture": "ethcc-2020/images/speakers_2020/Afri_Schoedon.jpg"
+    },
+    {
+        "company": "Ethereum Foundation",
+        "first_name": "Martin",
+        "last_name": "Lundfall",
+        "link": "https://twitter.com/martinlundfall",
+        "picture": "https://pbs.twimg.com/profile_images/969658522152521728/q-KnCs1i.jpg"
+    },
+    {
+        "company": "ConsenSys",
+        "first_name": "Tim",
+        "last_name": "Beiko",
+        "link": "https://twitter.com/timbeiko",
+        "picture": "https://drive.google.com/file/d/1w-Jp5CCjC-684s6tyR1fQiA7269mF_Ld/view?usp=sharing"
+    },
+    {
+        "company": "ENS",
+        "first_name": "Makoto",
+        "last_name": "Inoue",
+        "link": "https://twitter.com/makoto_inoue",
+        "picture": "https://pbs.twimg.com/profile_images/786179407710920704/ZwBzCmUz_400x400.jpg"
+    },
+    {
+        "company": "hermez.io",
+        "first_name": "Jordi",
+        "last_name": "Baylina",
+        "link": "https://twitter.com/jbaylina",
+        "picture": "https://10iyd315iql1cp7mg1jed84g-wpengine.netdna-ssl.com/wp-content/uploads/2019/10/jordi_baylina.jpg"
+    },
+    {
+        "company": "AAVE",
+        "first_name": "Emilio",
+        "last_name": "Frangella",
+        "link": "https://twitter.com/The3D_",
+        "picture": "https://pbs.twimg.com/profile_images/1260122514845577216/uMCS1VcE_400x400.jpg"
+    },
+    {
+        "company": "Monolith",
+        "first_name": "Mel",
+        "last_name": "Gelderman",
+        "link": "https://twitter.com/monolith_web3",
+        "picture": "ethcc-2020/images/speakers_2020/Mel_Gelderman.jpg"
+    },
+    {
+        "company": "BloxStaking.com",
+        "first_name": "Alon ",
+        "last_name": "Muroch",
+        "link": "https://twitter.com/AmMuroch",
+        "picture": "https://media-exp1.licdn.com/dms/image/C4D03AQHC8IH7kuWBOg/profile-displayphoto-shrink_400_400/0/1598963303180?e=1613001600&v=beta&t=mDSb5cViLLUIV0njd_wUzBbiKNivF9Vshw1ySkBe1xU"
+    },
+    {
+        "company": "Concordia University",
+        "first_name": "Mahsa",
+        "last_name": "Moosavi",
+        "link": "https://twitter.com/msv_mahsa",
+        "picture": "https://drive.google.com/file/d/1vit6HJfRBHf_Y-0_njKlSI4f0qQYRI2u/view?usp=sharing"
+    },
+    {
+        "company": "iExec",
+        "first_name": "Hadrien",
+        "last_name": "Croubois",
+        "link": "https://twitter.com/Amxx",
+        "picture": "https://pbs.twimg.com/profile_images/1128648389749563393/i11-DPwB_400x400.png"
+    },
+    {
+        "company": "Linum Labs",
+        "first_name": "Paula",
+        "last_name": "Pettit",
+        "link": "https://twitter.com/paulapbpb",
+        "picture": "https://i.ibb.co/1RQz0mp/Headshot-crop.jpg"
+    },
+    {
+        "company": "iExec",
+        "first_name": "Hadrien",
+        "last_name": "Croubois",
+        "link": "https://twitter.com/amxx",
+        "picture": "https://pbs.twimg.com/profile_images/1128648389749563393/i11-DPwB_400x400.png"
+    },
+    {
+        "company": "Winding Tree",
+        "first_name": "Maksim",
+        "last_name": "Izmaylov",
+        "link": "https://twitter.com/kvakes",
+        "picture": "https://maksimizmaylov.com/images/headshot.jpg"
+    },
+    {
+        "company": "WalletConnect",
+        "first_name": "Pedro",
+        "last_name": "Gomes",
+        "link": "https://twitter.com/pedrouid",
+        "picture": "https://pbs.twimg.com/profile_images/1154431219884580865/fYJVlT0h_400x400.png"
+    },
+    {
+        "company": "Status",
+        "first_name": "Simona",
+        "last_name": "Pop",
+        "link": "https://twitter.com/Sim_Pop",
+        "picture": "https://drive.google.com/file/d/1KEZGK1X9bo3a03ESy8RfvS4HlS52WAv8/view?usp=sharing"
+    },
+    {
+        "company": "Aave",
+        "first_name": "David",
+        "last_name": "Truong ",
+        "link": "https://twitter.com/daveytea",
+        "picture": "https://drive.google.com/file/d/1yuOIlG6IDxOhcWIwEvP1SJdV1p_SYvZR/view?usp=sharing"
+    },
+    {
+        "company": "Streamr",
+        "first_name": "Henri",
+        "last_name": "Pihkala",
+        "link": "https://twitter.com/henripihkala",
+        "picture": "https://cdn.streamr.com/landing-page-cdn/static/Henri-bd4924adaa8129faf101906bdb9701e7.jpg"
+    },
+    {
+        "company": "Aave",
+        "first_name": "Stani",
+        "last_name": "Kulechov",
+        "link": "https://twitter.com/StaniKulechov",
+        "picture": "https://drive.google.com/file/d/135YhUfnQ3cfvOqcTneiGjGF0gsJ0IIBX/view?usp=sharing"
+    },
+    {
+        "company": "Monolith",
+        "first_name": "Riccardo",
+        "last_name": "Persiani",
+        "link": "https://www.linkedin.com/in/riccardo-persiani/",
+        "picture": "https://i.ibb.co/Mszmyfk/bechelor-thesis-headshot.jpg"
+    },
+    {
+        "company": "The Graph",
+        "first_name": "Tegan",
+        "last_name": "Kline",
+        "link": "https://twitter.com/ https://www.linkedin.com/in/tegan-kline-17306b60 https://twitter.com/theklineventure ",
+        "picture": "https://drive.google.com/file/d/1w8VoYUSUO2I7y-TsWAtvp0ujD0Lmdya_/view?usp=sharing"
+    },
+    {
+        "company": "ConsenSys",
+        "first_name": "Nick",
+        "last_name": "Kritikos",
+        "link": "https://twitter.com/Kritikos",
+        "picture": "https://drive.google.com/file/d/17Af-YucCX--61Ey0SFG9rpB80OKTBirX/view?usp=sharing"
+    },
+    {
+        "company": "Ethereum Foundation",
+        "first_name": "Christian",
+        "last_name": "Reitwiessner",
+        "link": "https://twitter.com/twitter.com/ethchris",
+        "picture": "https://media-exp1.licdn.com/dms/image/C4E03AQF736-GqDn9QQ/profile-displayphoto-shrink_200_200/0?e=1612396800&v=beta&t=WaKatzhFDUUOn2l1Hne3M1V7XLdRdfKVWxXo9m6PJcE"
+    },
+    {
+        "company": "ConsenSys Diligence / Concordia University",
+        "first_name": "Shayan",
+        "last_name": "Eskandari",
+        "link": "https://shayan.es",
+        "picture": "https://pbs.twimg.com/profile_images/1263917069365587971/IT_0zycj_400x400.jpg"
+    },
+    {
+        "company": "DAppNode",
+        "first_name": "Eduardo",
+        "last_name": "Antuña",
+        "link": "https://twitter.com/eduadiez",
+        "picture": "https://i.imgur.com/S173AyL.png"
+    },
+    {
+        "company": " Near Foundation",
+        "first_name": "Sonja",
+        "last_name": "Prstec",
+        "link": "https://www.linkedin.com/in/sonja-prstec-mag-iur-89a0b324/",
+        "picture": "https://www.linkedin.com/in/sonja-prstec-mag-iur-89a0b324/"
+    },
+    {
+        "company": "Furucombo",
+        "first_name": "Chu",
+        "last_name": "Hsuan-Ting",
+        "link": "https://twitter.com/hsuantingchu",
+        "picture": "https://d.pr/i/hF2OlK"
+    },
+    {
+        "company": "Web3 Foundation",
+        "first_name": "Bruno",
+        "last_name": "Škvorc",
+        "link": "https://twitter.com/bitfalls",
+        "picture": "https://10iyd315iql1cp7mg1jed84g-wpengine.netdna-ssl.com/wp-content/uploads/2019/12/brunoskvorc.jpg"
+    },
+    {
+        "company": "NonFungible.com",
+        "first_name": "Gauthier",
+        "last_name": "Zuppinger",
+        "link": "https://twitter.com/Gauthier_Z",
+        "picture": "https://cdn.kang.fr/photo_master/210/601896/23334/1.jpg"
+    },
+    {
+        "company": "Axie Infinity",
+        "first_name": "Aleksander Leonard",
+        "last_name": "Larsen",
+        "link": "https://twitter.com/twitter.com/psycheout86",
+        "picture": "https://drive.google.com/file/d/1PkciOUVYOUZz_CzndYTFmlTNOvZAu0ls/view?usp=sharing"
+    },
+    {
+        "company": "Orchid",
+        "first_name": "Dr. Steven",
+        "last_name": "Waterhouse ",
+        "link": "https://twitter.com/deseventral",
+        "picture": "https://docs.google.com/document/d/1zR-pWkQdBgi5nyUNBe0jQwqfT6V2n7N3z-rFK2i8IX8/edit?usp=sharing"
+    }
 ]
