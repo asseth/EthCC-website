d2703eda2c2ca27931740f040617bd37b88bba3b
Bettina Boon Falleur <bettina.boonfalleur@gmail.com>
2020-12-13 21:14:29 +0100

    fixed display of speakers

--- js/speaker.js
+++ js/speaker.js
@@ -10,8 +10,8 @@ for (i = 0; i < speakers.length; i ++) {
     speakerTemplate.find('.firstName').wrap('<a href="'+speakers[i].link+'" target=_blank"></a>');
   }
 
-  speakerTemplate.find('.firstName').text(speakers[i].firstName);
-  speakerTemplate.find('.lastName').text(speakers[i].lastName);
+  speakerTemplate.find('.firstName').text(speakers[i].first_name);
+  speakerTemplate.find('.lastName').text(speakers[i].last_name);
   speakerTemplate.find('img').attr('src', speakers[i].picture);
   speakerTemplate.find('.company').text(speakers[i].company);
   speakersRow.append(speakerTemplate.html());
